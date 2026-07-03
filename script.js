76da273ffdb1e3469e09fc6ff6edad691cddcb2b
red <72036824+redx1109@users.noreply.github.com>
2026-07-03 14:35:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -436,7 +436,7 @@ function openAddToPlaylist(songId){
   document.getElementById('plModalContent').innerHTML=`
     <div style="font-size:15px;font-weight:600;margin-bottom:14px">Add to Playlist</div>
     <div onclick="toggleLike('${songId}');closePlModal();"
-      style="padding:10px 12px;border-radius:8px;cursor:pointer;margin-bottom:6px;background:var(--accent-d);display:flex;align-items:center;gap:10px;font-size:13px">
+      style="padding:10px 12px;border-radius:8px;cursor:pointer;margin-bottom:6px;background:var(--bg2);display:flex;align-items:center;gap:10px;font-size:13px">
       <i class="ti ti-heart" style="color:#e0353f"></i> Add to Favorites
     </div>
     ${names.length ? names.map(name=>`
